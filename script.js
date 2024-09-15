d29898db6cc3a10cab5aafd9bfb9598e6bbc0ec3
Johan Leo <johanlkoet@gmail.com>
2024-09-15 17:07:37 +0200

    make the initial animations

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const menuButton = document.querySelector("header h2");
 const dropDownMenu = document.querySelector("nav ul");
 const flowers = document.getElementById("flowers");
+const justABrain = document.getElementById("justabrain");
 const aboutMeTitle = document.getElementById("aboutme-h2");
 const illustrationsTitle = document.getElementById("illustrations-h2");
 const codingKoalaTitle = document.getElementById("codingkoala-h2");
@@ -10,17 +11,44 @@ const bar2 = document.getElementById("bar2");
 const bar3 = document.getElementById("bar3");
 
 window.addEventListener("scroll", () => {
-  let value = window.scrollY;
-  bar1.style.width = `${100 - value * 0.5}%`;
-  if (value >= 100) {
+  let scrollValue = window.scrollY;
+
+  bar1.style.width = `${100 - scrollValue * 0.5}%`;
+
+  if (scrollY > 900) {
+    bar2.style.width = `${100 - (scrollValue - 900) * 0.5}%`;
+  } else {
+    bar2.style.width = `100%`;
+  }
+
+  if (scrollValue >= 191) {
     aboutMeTitle.classList.add("move-left");
   } else {
     aboutMeTitle.classList.remove("move-left");
   }
-  if (value >= 575) {
+
+  if (scrollValue >= 1205) {
+    illustrationsTitle.classList.add("move-left");
+  } else {
+    illustrationsTitle.classList.remove("move-left");
+  }
+
+  if (scrollValue >= 1218) {
+    codingKoalaTitle.classList.add("move-left");
+  } else {
+    codingKoalaTitle.classList.remove("move-left");
+  }
+
+  if (scrollValue >= 575) {
     flowers.classList.add("move-right");
   } else {
     flowers.classList.remove("move-right");
+  }
+
+  if (scrollValue >= 1584) {
+    justABrain.classList.add("move-left");
+  } else {
+    justABrain.classList.remove("move-left");
   }
 });
 
